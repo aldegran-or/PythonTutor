b5c55453056aa694e33930d58b8d6189f387391f
Evgeny <aldegran@zen.aldegran.pp.ua>
2018-10-26 00:03:38 +0300

    дописана программа Кратность

--- l1/HW/multiplicity.py
+++ l1/HW/multiplicity.py
@@ -2,6 +2,16 @@
 #multiplicity.py
 import os
 os.system('clear')
-lst = input("n=")
-print(len(lst))
-print(lst[len(lst)-1])
+lst=[]
+print('программа выводит все числа из определённого диапазона кратные заданному')
+n = int(input("Начало диапазона="))
+k = int(input("Конец диапазона="))
+f = int(input("Кратность="))
+for i in range(n, k+1):
+    if i%f==0:
+        lst.append(i)
+#print(lst)        
+for j in range(len(lst)):
+    print(lst[j], end =" ")
+print()
+
